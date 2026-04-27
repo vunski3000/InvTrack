5714b105f27e63510376d0f6a6873b138eceb7f2
vunski3000 <vonczar00@gmail.com>
2026-04-27 12:51:13 +0800

    april27changes3

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABSE_PUBLISHABLE_KEY;
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
 console.log("Supabase URL:", supabaseUrl);
 console.log("Supabase Key Check:", supabaseKey ? "Key exists!" : "Key is missing!");
