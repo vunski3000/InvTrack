73b89c7ba94fff1fe77fe0ed162d2eb0f4e50dde
vunski3000 <vonczar00@gmail.com>
2026-07-12 12:41:52 +0800

    changesjuly

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1200
+  },
 });
